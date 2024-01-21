99fbca880d4cf410782940bce8274a0721bb616a
yuzonshen <80691695+yuzonshen@users.noreply.github.com>
2024-01-21 22:34:36 +0800

    Update 20240121 test

--- src/Startup.cs
+++ src/Startup.cs
@@ -52,15 +52,15 @@ namespace ContosoCrafts.WebSite
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
-                //endpoints.MapControllers();
-                //endpoints.MapBlazorHub();
+                endpoints.MapControllers();
+                endpoints.MapBlazorHub();
 
-                endpoints.MapGet("/products", (context) =>
-                {
-                    var products = app.ApplicationServices.GetService<JsonFileProductService>().GetProducts();
-                    var json = JsonSerializer.Serialize<IEnumerable<Product>>(products);
-                    return context.Response.WriteAsync(json);
-                });
+                //endpoints.MapGet("/products", (context) =>
+                //{
+                //    var products = app.ApplicationServices.GetService<JsonFileProductService>().GetProducts();
+                //    var json = JsonSerializer.Serialize<IEnumerable<Product>>(products);
+                //    return context.Response.WriteAsync(json);
+                //});
             });
         }
     }
